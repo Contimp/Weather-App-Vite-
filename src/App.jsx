f5fe32256c7c418bcc3298635d3c48e685ad0c7d
Contimp <contimariapamela@gmail.com>
2024-03-21 00:06:17 -0300

    SearchBar

--- src/App.jsx
+++ src/App.jsx
@@ -11,30 +11,34 @@ function App() {
   const [data, setData] = React.useState([]);
 
   function onSearch(ciudad) {
-    fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apikey}&units=metric`
-    )
-      .then((r) => r.json())
-      .then((recurso) => {
-        if (recurso.main !== undefined) {
-          const ciudad = {
-            min: Math.round(recurso.main.temp_min),
-            max: Math.round(recurso.main.temp_max),
-            img: recurso.weather[0].icon,
-            id: recurso.id,
-            wind: recurso.wind.speed,
-            temp: recurso.main.temp,
-            name: recurso.name,
-            weather: recurso.weather[0].main,
-            clouds: recurso.clouds.all,
-            latitud: recurso.coord.lat,
-            longitud: recurso.coord.lon,
-          };
-          setData((oldCities) => [...oldCities, ciudad]);
-        } else {
-          alert("Ciudad no encontrada");
-        }
-      });
+    if (data.length > 2) {
+      alert("You can't add more cities");
+    } else {
+      fetch(
+        `http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apikey}&units=metric`
+      )
+        .then((r) => r.json())
+        .then((recurso) => {
+          if (recurso.main !== undefined) {
+            const ciudad = {
+              min: Math.round(recurso.main.temp_min),
+              max: Math.round(recurso.main.temp_max),
+              img: recurso.weather[0].icon,
+              id: recurso.id,
+              wind: recurso.wind.speed,
+              temp: recurso.main.temp,
+              name: recurso.name,
+              weather: recurso.weather[0].main,
+              clouds: recurso.clouds.all,
+              latitud: recurso.coord.lat,
+              longitud: recurso.coord.lon,
+            };
+            setData((oldCities) => [...oldCities, ciudad]);
+          } else {
+            alert("Ciudad no encontrada");
+          }
+        });
+    }
   }
 
   function handleOnClose(id) {
@@ -58,7 +62,7 @@ function App() {
                 min={data[data.length - 1].min}
                 max={data[data.length - 1].max}
                 name={data[data.length - 1].name}
-                img={data[data.length - 1].icon}
+                img={data[data.length - 1].img}
               />
             )}
           </div>
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,10 +4,15 @@ import { FaSearch } from "react-icons/fa";
 
 export default function SearchBar({ onSearch }) {
   // acá va tu código
+
+  const [search, setSearch] = React.useState('')
+
   const handleOnSearch = () => {
-    const input = document.getElementById("searchInput");
-    onSearch(input.value);
-    input.value = "";
+    // const input = document.getElementById("searchInput");
+    // onSearch(input.value);
+    // input.value = "";
+    onSearch(search);
+    setSearch('');
   };
   return (
     <div>
@@ -16,6 +21,8 @@ export default function SearchBar({ onSearch }) {
         placeholder="Add City..."
         className={style.input}
         type="text"
+        value= {search}
+        onChange={(e)=>setSearch(e.target.value)}
       />
       <button className={style.button} onClick={handleOnSearch}>
         <FaSearch />
